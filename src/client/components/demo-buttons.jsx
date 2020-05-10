7df4a5ec4cf62b9d3669d66b258d463047db3f8e
Srikanth Reddy <srisrikanth703@gmail.com>
2020-05-10 14:03:50 +0530

    I made common chages

--- src/client/components/demo-buttons.jsx
+++ src/client/components/demo-buttons.jsx
@@ -9,7 +9,7 @@ import milligram from "milligram/dist/milligram.css"; // eslint-disable-line no-
 export const DemoButtons = () => (
   <div>
     <h6 styleName="custom.docs-header">
-      demo CSS modules with buttons from <a href="https://milligram.io/">milligram</a>
+      demo CSS modules with buttons from I created new branch <a href="https://milligram.io/">milligram</a>
     </h6>
     <div styleName="custom.docs-example">
       <a styleName="milligram.button" href="#">
